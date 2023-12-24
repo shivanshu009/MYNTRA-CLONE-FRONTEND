50fcf18f2dc8444268a10d1e12f2991fa7df7bd6
SHIVANSHU CHATURVEDI <154603464+shivanshu009@users.noreply.github.com>
2023-12-24 18:27:59 +0530

    Add files via upload

---
+++ prince.com/script.js
@@ -0,0 +1,12 @@
+// gsap.from("#p1box1 #p1btn",{
+//     y:100,
+//     delay:0.5,
+//     opacity:0,
+//     stagger:0.3
+gsap.from("#page2 .box",{
+    y:-100,
+    delay:0.5,
+    duration:1,
+    opacity:0,
+    stagger:0.3 
+})
